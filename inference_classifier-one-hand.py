5f9850ab06666f1a4487ab85653d30a3d9111e78
Loreto <emanoel.loreto@meta.com.br>
2023-05-18 18:48:16 -0300

    Wip - creating a second window to display the output from the recognized sign

--- inference_classifier-one-hand.py
+++ inference_classifier-one-hand.py
@@ -5,6 +5,9 @@ import mediapipe as mp
 import numpy as np
 import time
 
+import tkinter as tk
+import threading
+
 model_dict = pickle.load(open('./model.p', 'rb'))
 model = model_dict['model']
 
@@ -56,74 +59,104 @@ labels_dict = {
     'hello': 'Hello',
 }
 
-lastPredictedCharacter = ''
-contador = 0
+def inference_classifier():
+    lastPredictedCharacter = ''
+    contador = 0
+    
+    while True:
+        data_aux = []
+        x_ = []
+        y_ = []
 
-while True:
-    data_aux = []
-    x_ = []
-    y_ = []
+        ret, frame = cap.read()
 
-    ret, frame = cap.read()
+        H, W, _ = frame.shape
 
-    H, W, _ = frame.shape
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(frame_rgb)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame,  # image to draw
+                    hand_landmarks,  # model output
+                    mp_hands.HAND_CONNECTIONS,  # hand connections
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style()
+                )
 
-    results = hands.process(frame_rgb)
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(
-                frame,  # image to draw
-                hand_landmarks,  # model output
-                mp_hands.HAND_CONNECTIONS,  # hand connections
-                mp_drawing_styles.get_default_hand_landmarks_style(),
-                mp_drawing_styles.get_default_hand_connections_style()
-            )
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
 
-        for hand_landmarks in results.multi_hand_landmarks:
-            for i in range(len(hand_landmarks.landmark)):
-                x = hand_landmarks.landmark[i].x
-                y = hand_landmarks.landmark[i].y
+                    x_.append(x)
+                    y_.append(y)
 
-                x_.append(x)
-                y_.append(y)
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x - min(x_))
+                    data_aux.append(y - min(y_))
 
-            for i in range(len(hand_landmarks.landmark)):
-                x = hand_landmarks.landmark[i].x
-                y = hand_landmarks.landmark[i].y
-                data_aux.append(x - min(x_))
-                data_aux.append(y - min(y_))
+            x1 = int(min(x_) * W) - 10
+            y1 = int(min(y_) * H) - 10
 
-        x1 = int(min(x_) * W) - 10
-        y1 = int(min(y_) * H) - 10
+            x2 = int(max(x_) * W) - 10
+            y2 = int(max(y_) * H) - 10
 
-        x2 = int(max(x_) * W) - 10
-        y2 = int(max(y_) * H) - 10
+            if (len(data_aux) == 42):
+                prediction = model.predict([np.asarray(data_aux)])
 
-        if (len(data_aux) == 42):
-            prediction = model.predict([np.asarray(data_aux)])
+                predicted_character = labels_dict[prediction[0]]
 
-            predicted_character = labels_dict[prediction[0]]
+                if contador >= 600:
+                    print(predicted_character)
+                    # Update the label text in the tkinter window
+                    # label_var.set("CV2 processing completed")
+                    contador = 0
 
-            if contador >= 600:
-                print(predicted_character)
-                contador = 0
-
-            elif (predicted_character != lastPredictedCharacter):
-                contador = 0
-            else:
-                contador += 20
-                cv2.rectangle(frame, (0, 40), (contador, 40), (124,252,0), 30)
+                elif (predicted_character != lastPredictedCharacter):
+                    contador = 0
+                else:
+                    contador += 20
+                    cv2.rectangle(frame, (0, 40), (contador, 40), (124,252,0), 30)
 
 
-            lastPredictedCharacter = predicted_character
+                lastPredictedCharacter = predicted_character
 
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
-            cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3, cv2.LINE_AA)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+                cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3, cv2.LINE_AA)
 
-    cv2.imshow('frame', frame)
-    cv2.waitKey(1)
+        cv2.imshow('frame', frame)
+        cv2.waitKey(1)
 
-cap.release()
-cv2.destroyAllWindows()
+def display_tkinter():
+    # Create a new window
+    window = tk.Tk()
+
+    # Set the window title
+    window.title("Output sign recognized")
+
+    # Set the window Width x Height
+    window.geometry("600x400")
+
+    # Create a StringVar to store the label text
+    label_var = tk.StringVar()
+    label_var.set("Waiting for CV2 processing...")
+
+    # Create a label widget to display the text
+    label = tk.Label(window, textvariable=label_var)
+    label.pack()
+
+    # Run the tkinter event loop
+    window.mainloop()
+
+    # Run the tkinter event loop
+    window.mainloop()
+
+# Create and start the threads for inference_classifier cv2 and tkinter windows
+cv2_thread = threading.Thread(target=inference_classifier)
+tkinter_thread = threading.Thread(target=display_tkinter)
+cv2_thread.start()
+tkinter_thread.start()
